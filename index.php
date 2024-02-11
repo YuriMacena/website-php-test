ed21c79426c01d80b909d05638775ea81aea1e0c
Yuri Macena <yurimacena10@gmail.com>
2024-02-10 21:48:25 -0300

    Início do teste

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-    echo 'Hello, world!'
+    $pags = ['home'=>'Minha página home aqui!', 'about'=>'Estou na página sobre', 'contact'=>''];
+
+    $pags['contact'] = '<form><input type="text" placeholder="Seu nome..." /></form>';
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Dinâmico</title>
+</head>
+<body>
+    <?php
+        echo $pags['contact'];
+    ?>
+</body>
+</html>
